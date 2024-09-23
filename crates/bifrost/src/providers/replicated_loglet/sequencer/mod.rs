5b4540d8cdf67b2613057e62866361b0e13ef0ec
Muhamad Awad <muhamad@restate.dev>
2024-09-23 14:25:03 +0200

    scaffolding appender

--- crates/bifrost/src/providers/replicated_loglet/sequencer/mod.rs
+++ crates/bifrost/src/providers/replicated_loglet/sequencer/mod.rs
@@ -8,4 +8,31 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+mod append;
 mod node;
+
+use restate_types::{replicated_loglet::ReplicatedLogletId, GenerationalNodeId};
+
+use crate::loglet::util::TailOffsetWatch;
+
+/// A sharable part of the sequencer state. This is shared with node workers
+#[derive(Debug)]
+pub(crate) struct SequencerGlobalState {
+    node_id: GenerationalNodeId,
+    loglet_id: ReplicatedLogletId,
+    committed_tail: TailOffsetWatch,
+}
+
+impl SequencerGlobalState {
+    pub fn node_id(&self) -> &GenerationalNodeId {
+        &self.node_id
+    }
+
+    pub fn loglet_id(&self) -> &ReplicatedLogletId {
+        &self.loglet_id
+    }
+
+    pub fn committed_tail(&self) -> &TailOffsetWatch {
+        &self.committed_tail
+    }
+}
