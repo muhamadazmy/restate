dce213adb5a6ebe19a5d5f13db8fa15e4604a2b3
Muhamad Awad <muhamad@restate.dev>
2024-09-26 13:14:47 +0200

    [ReplicatedLoglet] Record Cache
    Summary:
    Initial implementation of a shared record cache

--- crates/bifrost/src/providers/replicated_loglet/record_cache.rs
+++ crates/bifrost/src/providers/replicated_loglet/record_cache.rs
@@ -8,6 +8,13 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
+use std::ops::RangeBounds;
+
+use restate_types::{
+    logs::{LogletOffset, Record},
+    replicated_loglet::ReplicatedLogletId,
+};
+
 /// A placeholder for a global record cache.
 ///
 /// This can be safely shared between all ReplicatedLoglet(s) and the LocalSequencers or the
@@ -22,4 +29,47 @@ impl RecordCache {
     pub fn new(_memory_budget_bytes: usize) -> Self {
         Self {}
     }
+
+    /// Writes a record to cache.
+    pub fn push(_loglet_id: ReplicatedLogletId, _offset: LogletOffset, _record: Record) {
+        todo!()
+    }
+
+    /// Extend cache with records
+    pub fn extend<I: AsRef<[Record]>>(
+        &self,
+        _loglet_id: ReplicatedLogletId,
+        _first_offset: LogletOffset,
+        _records: I,
+    ) {
+        todo!()
+    }
+
+    /// Get a for given loglet id and offset.
+    pub fn get(
+        &self,
+        _loglet_id: ReplicatedLogletId,
+        _offset: ReplicatedLogletId,
+    ) -> Option<&Record> {
+        todo!()
+    }
+
+    /// Get range of records from cache.
+    pub fn get_range<R>(&self, _loglet_id: ReplicatedLogletId, _range: R) -> Vec<CachedRecords>
+    where
+        R: RangeBounds<LogletOffset>,
+    {
+        todo!()
+    }
+}
+
+pub enum CachedRecords {
+    Gap {
+        from: LogletOffset,
+        to: LogletOffset,
+    },
+    Batch {
+        first_offset: LogletOffset,
+        records: Vec<Record>,
+    },
 }
