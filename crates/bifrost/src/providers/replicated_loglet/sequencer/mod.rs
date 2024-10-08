91d34b14065985fd21ee4082e2da83e6306c1d8b
Muhamad Awad <muhamad@restate.dev>
2024-10-08 12:08:56 +0200

    [Sequencer] Add traces and metrics to the sequencer

--- crates/bifrost/src/providers/replicated_loglet/sequencer/mod.rs
+++ crates/bifrost/src/providers/replicated_loglet/sequencer/mod.rs
@@ -27,13 +27,13 @@ use restate_types::{
     GenerationalNodeId,
 };
 
+use self::appender::SequencerAppender;
 use super::{
     log_server_manager::RemoteLogServerManager,
     record_cache::RecordCache,
     replication::spread_selector::{SelectorStrategy, SpreadSelector},
 };
 use crate::loglet::{util::TailOffsetWatch, LogletCommit, OperationError};
-use appender::SequencerAppender;
 
 #[derive(thiserror::Error, Debug)]
 pub enum SequencerError {
@@ -232,15 +232,24 @@ impl<T: TransportConnect> Sequencer<T> {
 trait BatchExt {
     /// tail computes inflight tail after this batch is committed
     fn last_offset(&self, first_offset: LogletOffset) -> Result<LogletOffset, SequencerError>;
+    fn estimated_encode_size(&self) -> usize;
 }
 
-impl BatchExt for Arc<[Record]> {
+impl<T: AsRef<[Record]>> BatchExt for T {
     fn last_offset(&self, first_offset: LogletOffset) -> Result<LogletOffset, SequencerError> {
-        let len = u32::try_from(self.len()).map_err(|_| SequencerError::InvalidBatchLength)?;
+        let len =
+            u32::try_from(self.as_ref().len()).map_err(|_| SequencerError::InvalidBatchLength)?;
 
         first_offset
             .checked_add(len - 1)
             .map(LogletOffset::from)
             .ok_or(SequencerError::LogletOffsetExhausted)
+    }
+
+    fn estimated_encode_size(&self) -> usize {
+        self.as_ref()
+            .iter()
+            .map(|r| r.estimated_encode_size())
+            .sum()
     }
 }
