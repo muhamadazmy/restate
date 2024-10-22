fd9aeb0a759eff90b39490f53f626f47dc105aea
Muhamad Awad <muhamad@restate.dev>
2024-10-22 11:51:06 +0200

    Stablize CI tests
    Summary:
    This fixes the issue with ports conflicts by marking used
    ports under /tmp/restate_test_ports
    
    fixes #2160

--- crates/local-cluster-runner/src/lib.rs
+++ crates/local-cluster-runner/src/lib.rs
@@ -1,14 +1,22 @@
 use std::{
+    fs,
     future::Future,
     io,
     net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener},
 };
 
+use rand::Rng;
 use tokio::signal::unix::SignalKind;
 use tracing::info;
 
 pub mod cluster;
 pub mod node;
+
+/// Used to store marker files of "used" ports to avoid confilcts
+///
+/// Please make sure the path `$TMP_DIR/restate_test_ports` is deleted
+/// before starting tests
+const PORTS_POOL: &str = "restate_test_ports";
 
 pub fn shutdown() -> impl Future<Output = &'static str> {
     let mut interrupt = tokio::signal::unix::signal(SignalKind::interrupt())
@@ -28,8 +36,38 @@ pub fn shutdown() -> impl Future<Output = &'static str> {
 }
 
 pub fn random_socket_address() -> io::Result<SocketAddr> {
-    let listener = TcpListener::bind((IpAddr::V4(Ipv4Addr::LOCALHOST), 0))?;
-    let socket_addr = listener.local_addr()?;
+    let base_path = std::env::temp_dir().join(PORTS_POOL);
+    // this can happen repeatedly but it's a test so it's okay
+    fs::create_dir_all(&base_path)?;
+    let mut rng = rand::thread_rng();
+    loop {
+        let port = rng.gen_range(1025..u16::MAX);
+        let port_file = base_path.join(port.to_string());
 
-    Ok(socket_addr)
+        match fs::OpenOptions::new()
+            .create(true)
+            .create_new(true)
+            .truncate(true)
+            .write(true)
+            .open(port_file)
+        {
+            Ok(_) => {}
+            Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
+                // port already reserved!
+                // try another port
+                continue;
+            }
+            Err(err) => return Err(err),
+        }
+
+        // just make sure this port is actually free
+        match TcpListener::bind((IpAddr::V4(Ipv4Addr::LOCALHOST), port)) {
+            Ok(listener) => return listener.local_addr(),
+            Err(err) if err.kind() == io::ErrorKind::AlreadyExists => {
+                // try again !
+                continue;
+            }
+            Err(err) => return Err(err),
+        }
+    }
 }
