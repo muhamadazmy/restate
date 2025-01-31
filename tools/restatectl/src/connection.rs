85f7dd52b7f1398307bb80542884fd009cbc01f9
Muhamad Awad <muhamad@restate.dev>
2025-01-31 18:04:36 +0100

    Streamline restatectl connection to cluster
    Summary:
    Make it easier and cleaner to run the restatectl command
    by accepting the restate nodes addresses via a unified argument `--address`
    
    - Support passing multiple addresses
    - The command will try to fetch the nodes configuration from the first reachable node
    - All other subcommands can then use the connection info to use the nodes configuration directly
      or to connect to a specific node role

---
+++ tools/restatectl/src/connection.rs
@@ -0,0 +1,310 @@
+// Copyright (c) 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use std::{cmp::Ordering, collections::HashMap, fmt::Display, future::Future, sync::Arc};
+
+use cling::{prelude::Parser, Collect};
+use itertools::{Either, Itertools};
+use rand::seq::IteratorRandom;
+use tokio::sync::{Mutex, MutexGuard};
+use tonic::{codec::CompressionEncoding, transport::Channel, Response, Status};
+use tracing::debug;
+
+use restate_core::protobuf::node_ctl_svc::{
+    node_ctl_svc_client::NodeCtlSvcClient, GetMetadataRequest,
+};
+use restate_types::{
+    logs::metadata::Logs,
+    net::AdvertisedAddress,
+    nodes_config::{NodesConfiguration, Role},
+    protobuf::common::{MetadataKind, NodeStatus},
+    storage::{StorageCodec, StorageDecode, StorageDecodeError},
+    Version, Versioned,
+};
+
+use crate::util::grpc_channel;
+
+#[derive(Clone, Parser, Collect, Debug)]
+pub struct ConnectionInfo {
+    // todo: rename this to be a node address for reusability across commands
+    /// Cluster Controller address
+    #[clap(
+        long="address",
+        value_hint = clap::ValueHint::Url,
+        default_value = "http://localhost:5122/",
+        env = "RESTATE_ADDRESS",
+        global = true
+    )]
+    pub addresses: Vec<AdvertisedAddress>,
+
+    /// Sync metadata from metadata store first
+    #[arg(long)]
+    pub sync_metadata: bool,
+
+    #[clap(skip)]
+    nodes_configuration: Arc<Mutex<Option<NodesConfiguration>>>,
+
+    #[clap(skip)]
+    logs: Arc<Mutex<Option<Logs>>>,
+
+    #[clap(skip)]
+    cache: Arc<Mutex<HashMap<AdvertisedAddress, Channel>>>,
+}
+
+impl ConnectionInfo {
+    /// Gets NodesConfiguration object. This function tries all provided addresses and makes sure
+    /// nodes configuration is cached.
+    pub async fn get_nodes_configuration(&self) -> Result<NodesConfiguration, ConnectionInfoError> {
+        let guard = self.nodes_configuration.lock().await;
+
+        // get nodes configuration will always use the addresses seed
+        // provided via the cmdline
+        self.get_latest(
+            self.addresses.iter(),
+            GetMetadataRequest {
+                kind: MetadataKind::NodesConfiguration.into(),
+                sync: false,
+            },
+            guard,
+        )
+        .await
+    }
+
+    /// Gets Logs object. This function tries all provided addresses and makes sure
+    /// nodes logs is cached.
+    pub async fn get_logs(&self) -> Result<Logs, ConnectionInfoError> {
+        let nodes_config = self.get_nodes_configuration().await?;
+
+        let guard = self.logs.lock().await;
+
+        let cluster_size = nodes_config.iter().count();
+        // try 50% + 1 of cluster size to find the latest logs version
+        let try_nodes = nodes_config
+            .iter()
+            .map(|(_, node)| &node.address)
+            .choose_multiple(&mut rand::thread_rng(), (cluster_size / 2) + 1);
+
+        self.get_latest(
+            try_nodes.into_iter(),
+            GetMetadataRequest {
+                kind: MetadataKind::Logs.into(),
+                sync: false,
+            },
+            guard,
+        )
+        .await
+    }
+
+    /// Gets NodesConfiguration object. This function tries all provided addresses and makes sure
+    /// nodes configuration is cached.
+    async fn get_latest<T>(
+        &self,
+        addresses: impl Iterator<Item = &AdvertisedAddress>,
+        request: GetMetadataRequest,
+        mut guard: MutexGuard<'_, Option<T>>,
+    ) -> Result<T, ConnectionInfoError>
+    where
+        T: StorageDecode + Versioned + Clone,
+    {
+        if let Some(meta) = &*guard {
+            return Ok(meta.clone());
+        }
+
+        if self.addresses.is_empty() {
+            return Err(ConnectionInfoError::NoAvailableNodes(NoRoleError(None)));
+        }
+
+        let mut latest_meta: Option<T> = None;
+        let mut answer = false;
+        let mut errors = NodesErrors::default();
+        let mut cache = self.cache.lock().await;
+
+        for address in addresses {
+            let channel = cache.entry(address.clone()).or_insert_with(|| {
+                debug!("connecting to {address}");
+                grpc_channel(address.clone())
+            });
+
+            let mut client = NodeCtlSvcClient::new(channel.clone())
+                .accept_compressed(CompressionEncoding::Gzip)
+                .send_compressed(CompressionEncoding::Gzip);
+
+            let response = match client.get_ident(()).await {
+                Ok(response) => response.into_inner(),
+                Err(status) => {
+                    errors.error(address.clone(), status);
+                    continue;
+                }
+            };
+
+            // node is reachable and answered
+            answer = true;
+            if response.status != NodeStatus::Alive as i32 {
+                // node did not join the cluster yet.
+                continue;
+            }
+
+            if Version::from(response.nodes_config_version)
+                <= latest_meta
+                    .as_ref()
+                    .map(|c| c.version())
+                    .unwrap_or(Version::INVALID)
+            {
+                // has older version that we have.
+                continue;
+            }
+
+            let mut response = match client.get_metadata(request).await {
+                Ok(response) => response.into_inner(),
+                Err(status) => {
+                    errors.error(address.clone(), status);
+                    continue;
+                }
+            };
+
+            let meta = StorageCodec::decode::<T, _>(&mut response.encoded).map_err(|err| {
+                ConnectionInfoError::InvalidNodesConfiguration(address.clone(), err)
+            })?;
+
+            if meta.version()
+                > latest_meta
+                    .as_ref()
+                    .map(|c| c.version())
+                    .unwrap_or(Version::INVALID)
+            {
+                latest_meta = Some(meta);
+            }
+        }
+
+        if !answer {
+            // all nodes have returned error
+            return Err(ConnectionInfoError::NodesErrors(errors));
+        }
+
+        *guard = latest_meta.clone();
+        latest_meta.ok_or(ConnectionInfoError::MissingMetadata)
+    }
+
+    pub async fn try_each<F, T, Fut>(
+        &self,
+        role: Option<Role>,
+        mut closure: F,
+    ) -> Result<Response<T>, ConnectionInfoError>
+    where
+        F: FnMut(Channel) -> Fut,
+        Fut: Future<Output = Result<Response<T>, Status>>,
+    {
+        let nodes_config = self.get_nodes_configuration().await?;
+        let mut channels = self.cache.lock().await;
+
+        let iterator = match role {
+            Some(role) => Either::Left(nodes_config.iter_role(role)),
+            None => Either::Right(nodes_config.iter()),
+        }
+        .sorted_by(|a, b| {
+            // nodes for which we already have open channels get higher precedence.
+            match (
+                channels.contains_key(&a.1.address),
+                channels.contains_key(&b.1.address),
+            ) {
+                (true, false) => Ordering::Less,
+                (false, true) => Ordering::Greater,
+                (_, _) => a.0.cmp(&b.0),
+            }
+        });
+
+        let mut errors = NodesErrors::default();
+
+        for (_, node) in iterator {
+            // avoid creating new channels on each iteration. Instead cheaply copy the channels
+            let channel = channels
+                .entry(node.address.clone())
+                .or_insert_with(|| grpc_channel(node.address.clone()));
+
+            let result = closure(channel.clone()).await;
+            match result {
+                Ok(response) => return Ok(response),
+                Err(status) => {
+                    errors.error(node.address.clone(), status);
+                }
+            }
+        }
+
+        if errors.is_empty() {
+            Err(ConnectionInfoError::NoAvailableNodes(NoRoleError(role)))
+        } else {
+            Err(ConnectionInfoError::NodesErrors(errors))
+        }
+    }
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConnectionInfoError {
+    #[error("Could not retrieve cluster metadata. Has the cluster been provisioned yet?")]
+    MissingMetadata,
+
+    #[error("Failed to decode nodes configuration from node {0}: {1}")]
+    InvalidNodesConfiguration(AdvertisedAddress, StorageDecodeError),
+
+    #[error(transparent)]
+    NodesErrors(NodesErrors),
+
+    #[error(transparent)]
+    NoAvailableNodes(NoRoleError),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub struct NoRoleError(Option<Role>);
+
+impl Display for NoRoleError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.0 {
+            Some(role) => {
+                write!(f, "No available {role} nodes to satisfy the request")?;
+            }
+            None => {
+                write!(f, "No available nodes to satisfy the request")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct NodesErrors {
+    node_status: Vec<(AdvertisedAddress, Status)>,
+}
+
+impl NodesErrors {
+    fn error(&mut self, node: AdvertisedAddress, status: Status) {
+        self.node_status.push((node, status));
+    }
+
+    fn is_empty(&self) -> bool {
+        self.node_status.is_empty()
+    }
+}
+
+impl Display for NodesErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Encountered multiple errors:")?;
+        for (address, status) in &self.node_status {
+            writeln!(f, " - {address} -> {status}")?;
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for NodesErrors {
+    fn description(&self) -> &str {
+        "aggregated nodes error"
+    }
+}
