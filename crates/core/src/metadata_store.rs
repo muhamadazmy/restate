4cff35a0d84ee5b8dd50a39757ab0d69aab5c1fd
Muhamad Awad <muhamad@restate.dev>
2025-02-05 11:51:18 +0100

    Implement Telemetry Metrics for Metadata Server
    Summary:
    This pull request introduces telemetry metrics to the metadata server, enhancing monitoring and performance analysis capabilities.
    
    Fixes #2605

--- crates/core/src/metadata_store.rs
+++ crates/core/src/metadata_store.rs
@@ -11,11 +11,14 @@
 pub mod providers;
 mod test_util;
 
-#[cfg(any(test, feature = "test-util"))]
-use crate::metadata_store::test_util::InMemoryMetadataStore;
+use std::future::Future;
+use std::sync::Arc;
+
 use async_trait::async_trait;
 use bytes::{Bytes, BytesMut};
 use bytestring::ByteString;
+use tracing::debug;
+
 use restate_types::errors::{
     BoxedMaybeRetryableError, GenericError, IntoMaybeRetryable, MaybeRetryableError,
 };
@@ -24,9 +27,17 @@ use restate_types::nodes_config::NodesConfiguration;
 use restate_types::retries::RetryPolicy;
 use restate_types::storage::{StorageCodec, StorageDecode, StorageEncode, StorageEncodeError};
 use restate_types::{flexbuffers_storage_encode_decode, Version, Versioned};
-use std::future::Future;
-use std::sync::Arc;
-use tracing::debug;
+
+#[cfg(any(test, feature = "test-util"))]
+use crate::metadata_store::test_util::InMemoryMetadataStore;
+use crate::metric_definitions::{
+    METADATA_CLIENT_DELETE_DURATION, METADATA_CLIENT_DELETE_TOTAL_ERROR,
+    METADATA_CLIENT_DELETE_TOTAL_SUCCESS, METADATA_CLIENT_GET_DURATION,
+    METADATA_CLIENT_GET_TOTAL_ERROR, METADATA_CLIENT_GET_TOTAL_SUCCESS,
+    METADATA_CLIENT_GET_VERSION_DURATION, METADATA_CLIENT_GET_VERSION_TOTAL_ERROR,
+    METADATA_CLIENT_GET_VERSION_TOTAL_SUCCESS, METADATA_CLIENT_PUT_DURATION,
+    METADATA_CLIENT_PUT_TOTAL_ERROR, METADATA_CLIENT_PUT_TOTAL_SUCCESS,
+};
 
 #[derive(Debug, thiserror::Error)]
 pub enum ReadError {
@@ -263,6 +274,21 @@ impl<T: ProvisionedMetadataStore + Sync> MetadataStore for T {
     }
 }
 
+macro_rules! metrics_helper {
+    (@success=$ok:ident, @error=$err:ident, @duration=$dur:ident, $block:block) => {{
+        let start_time = tokio::time::Instant::now();
+        let result = $block;
+        metrics::histogram!($dur).record(start_time.elapsed());
+        if result.is_ok() {
+            metrics::counter!($ok).increment(1);
+        } else {
+            metrics::counter!($err).increment(1);
+        }
+
+        result
+    }};
+}
+
 /// Metadata store client which allows storing [`Versioned`] values into a [`MetadataStore`].
 #[derive(Clone)]
 pub struct MetadataStoreClient {
@@ -299,28 +325,42 @@ impl MetadataStoreClient {
         &self,
         key: ByteString,
     ) -> Result<Option<T>, ReadError> {
-        let value = self.inner.get(key).await?;
+        metrics_helper!(
+            @success=METADATA_CLIENT_GET_TOTAL_SUCCESS,
+            @error=METADATA_CLIENT_GET_TOTAL_ERROR,
+            @duration=METADATA_CLIENT_GET_DURATION,
+            {
+                let value = self.inner.get(key).await?;
 
-        if let Some(mut versioned_value) = value {
-            let value = StorageCodec::decode::<T, _>(&mut versioned_value.value)
-                .map_err(|err| ReadError::Codec(err.into()))?;
+                if let Some(mut versioned_value) = value {
+                    let value = StorageCodec::decode::<T, _>(&mut versioned_value.value)
+                        .map_err(|err| ReadError::Codec(err.into()))?;
 
-            assert_eq!(
-                versioned_value.version,
-                value.version(),
-                "versions must align"
-            );
+                    assert_eq!(
+                        versioned_value.version,
+                        value.version(),
+                        "versions must align"
+                    );
 
-            Ok(Some(value))
-        } else {
-            Ok(None)
-        }
+                    Ok(Some(value))
+                } else {
+                    Ok(None)
+                }
+            }
+        )
     }
 
     /// Gets the current version for the given key. If key-value pair is not present, then return
     /// [`None`].
     pub async fn get_version(&self, key: ByteString) -> Result<Option<Version>, ReadError> {
-        self.inner.get_version(key).await
+        metrics_helper!(
+            @success=METADATA_CLIENT_GET_VERSION_TOTAL_SUCCESS,
+            @error=METADATA_CLIENT_GET_VERSION_TOTAL_ERROR,
+            @duration=METADATA_CLIENT_GET_VERSION_DURATION,
+            {
+                self.inner.get_version(key).await
+            }
+        )
     }
 
     /// Puts the versioned value under the given key following the provided precondition. If the
@@ -334,10 +374,17 @@ impl MetadataStoreClient {
     where
         T: Versioned + StorageEncode,
     {
-        let versioned_value =
-            serialize_value(value).map_err(|err| WriteError::Codec(err.into()))?;
+        metrics_helper!(
+            @success=METADATA_CLIENT_PUT_TOTAL_SUCCESS,
+            @error=METADATA_CLIENT_PUT_TOTAL_ERROR,
+            @duration=METADATA_CLIENT_PUT_DURATION,
+            {
+                let versioned_value =
+                serialize_value(value).map_err(|err| WriteError::Codec(err.into()))?;
 
-        self.inner.put(key, versioned_value, precondition).await
+                self.inner.put(key, versioned_value, precondition).await
+            }
+        )
     }
 
     /// Deletes the key-value pair for the given key following the provided precondition. If the
@@ -347,7 +394,14 @@ impl MetadataStoreClient {
         key: ByteString,
         precondition: Precondition,
     ) -> Result<(), WriteError> {
-        self.inner.delete(key, precondition).await
+        metrics_helper!(
+            @success=METADATA_CLIENT_DELETE_TOTAL_SUCCESS,
+            @error=METADATA_CLIENT_DELETE_TOTAL_ERROR,
+            @duration=METADATA_CLIENT_DELETE_DURATION,
+            {
+                self.inner.delete(key, precondition).await
+            }
+        )
     }
 
     /// Gets the value under the specified key or inserts a new value if it is not present into the
--- crates/core/src/metric_definitions.rs
+++ crates/core/src/metric_definitions.rs
@@ -8,7 +8,7 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0.
 
-use metrics::{describe_counter, Unit};
+use metrics::{describe_counter, describe_histogram, Unit};
 
 // value of label `kind` in TC_SPAWN are defined in [`crate::TaskKind`].
 pub const TC_SPAWN: &str = "restate.task_center.spawned.total";
@@ -18,15 +18,110 @@ pub const TC_FINISHED: &str = "restate.task_center.finished.total";
 pub const TC_STATUS_COMPLETED: &str = "completed";
 pub const TC_STATUS_FAILED: &str = "failed";
 
+pub(crate) const METADATA_CLIENT_GET_DURATION: &str = "restate.metadata_client.get.duration";
+pub(crate) const METADATA_CLIENT_GET_VERSION_DURATION: &str =
+    "restate.metadata_client.get_version.duration";
+pub(crate) const METADATA_CLIENT_PUT_DURATION: &str = "restate.metadata_client.put.duration";
+pub(crate) const METADATA_CLIENT_DELETE_DURATION: &str = "restate.metadata_client.delete.duration";
+
+pub(crate) const METADATA_CLIENT_GET_TOTAL_SUCCESS: &str =
+    "restate.metadata_client.get.total.success";
+pub(crate) const METADATA_CLIENT_GET_VERSION_TOTAL_SUCCESS: &str =
+    "restate.metadata_client.get_version.total.success";
+pub(crate) const METADATA_CLIENT_PUT_TOTAL_SUCCESS: &str =
+    "restate.metadata_client.put.total.success";
+pub(crate) const METADATA_CLIENT_DELETE_TOTAL_SUCCESS: &str =
+    "restate.metadata_client.delete.total.success";
+
+pub(crate) const METADATA_CLIENT_GET_TOTAL_ERROR: &str = "restate.metadata_client.get.total.error";
+pub(crate) const METADATA_CLIENT_GET_VERSION_TOTAL_ERROR: &str =
+    "restate.metadata_client.get_version.total.error";
+pub(crate) const METADATA_CLIENT_PUT_TOTAL_ERROR: &str = "restate.metadata_client.put.total.error";
+pub(crate) const METADATA_CLIENT_DELETE_TOTAL_ERROR: &str =
+    "restate.metadata_client.delete.total.error";
+
 pub fn describe_metrics() {
     describe_counter!(
         TC_SPAWN,
         Unit::Count,
         "Total tasks spawned by the task center"
     );
+
     describe_counter!(
         TC_FINISHED,
         Unit::Count,
         "Number of tasks that finished with 'status'"
+    );
+
+    describe_histogram!(
+        METADATA_CLIENT_GET_DURATION,
+        Unit::Seconds,
+        "Metadata client get request duration in seconds"
+    );
+
+    describe_histogram!(
+        METADATA_CLIENT_GET_VERSION_DURATION,
+        Unit::Seconds,
+        "Metadata client get_version request duration in seconds"
+    );
+
+    describe_histogram!(
+        METADATA_CLIENT_PUT_DURATION,
+        Unit::Seconds,
+        "Metadata client put request duration in seconds"
+    );
+
+    describe_histogram!(
+        METADATA_CLIENT_DELETE_DURATION,
+        Unit::Seconds,
+        "Metadata client delete request duration in seconds"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_GET_TOTAL_SUCCESS,
+        Unit::Count,
+        "Metadata client get request success count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_GET_VERSION_TOTAL_SUCCESS,
+        Unit::Count,
+        "Metadata client get_version request success count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_PUT_TOTAL_SUCCESS,
+        Unit::Count,
+        "Metadata client put request success count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_DELETE_TOTAL_SUCCESS,
+        Unit::Count,
+        "Metadata client delete request success count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_GET_TOTAL_ERROR,
+        Unit::Count,
+        "Metadata client get request error count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_GET_VERSION_TOTAL_ERROR,
+        Unit::Count,
+        "Metadata client get_version request error count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_PUT_TOTAL_ERROR,
+        Unit::Count,
+        "Metadata client put request error count"
+    );
+
+    describe_counter!(
+        METADATA_CLIENT_DELETE_TOTAL_ERROR,
+        Unit::Count,
+        "Metadata client delete request error count"
     );
 }
