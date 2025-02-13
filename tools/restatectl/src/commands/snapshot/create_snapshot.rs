6bb74bb43d82f9a05f62eef781fda7b4a9658158
Muhamad Awad <muhamad@restate.dev>
2025-02-13 13:53:32 +0100

    Unify the `range input` for some ids
    Summary:
    - Support parsing an "range" input.
    - Handle id range in some common operations
      - `logs describe`
      - `logs find-tail`
      - `logs reconfigure`
      - `snapshots create-snapshot`

--- tools/restatectl/src/commands/snapshot/create_snapshot.rs
+++ tools/restatectl/src/commands/snapshot/create_snapshot.rs
@@ -11,6 +11,7 @@
 use anyhow::Context;
 use cling::prelude::*;
 use tonic::codec::CompressionEncoding;
+use tracing::error;
 
 use restate_admin::cluster_controller::protobuf::cluster_ctrl_svc_client::ClusterCtrlSvcClient;
 use restate_admin::cluster_controller::protobuf::CreatePartitionSnapshotRequest;
@@ -18,23 +19,35 @@ use restate_cli_util::c_println;
 use restate_types::nodes_config::Role;
 
 use crate::connection::ConnectionInfo;
+use crate::util::RangeParam;
 
 #[derive(Run, Parser, Collect, Clone, Debug)]
 #[clap(visible_alias = "create")]
 #[cling(run = "create_snapshot")]
 pub struct CreateSnapshotOpts {
-    /// The partition to snapshot
-    #[arg(short, long)]
-    partition_id: u16,
+    /// The partition id or range to snapshot, e.g. "0", "1-4"
+    #[arg(required = true)]
+    partition_id: Vec<RangeParam>,
 }
 
 async fn create_snapshot(
     connection: &ConnectionInfo,
     opts: &CreateSnapshotOpts,
 ) -> anyhow::Result<()> {
-    let request = CreatePartitionSnapshotRequest {
-        partition_id: opts.partition_id as u32,
-    };
+    for partition_id in opts.partition_id.iter().flatten() {
+        // make sure partition_id fits in a u16
+        if let Err(err) = inner_create_snapshot(connection, partition_id).await {
+            error!("Failed to create snapshot for partition {partition_id}: {err}");
+        }
+    }
+    Ok(())
+}
+
+async fn inner_create_snapshot(
+    connection: &ConnectionInfo,
+    partition_id: u32,
+) -> anyhow::Result<()> {
+    let request = CreatePartitionSnapshotRequest { partition_id };
 
     let response = connection
         .try_each(Some(Role::Admin), |channel| async {
@@ -48,7 +61,10 @@ async fn create_snapshot(
         .context("Failed to request snapshot")?
         .into_inner();
 
-    c_println!("Snapshot created: {}", response.snapshot_id);
+    c_println!(
+        "Snapshot created for partition {partition_id}: {}",
+        response.snapshot_id
+    );
 
     Ok(())
 }
