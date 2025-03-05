75cde5eccfebaccb7dcb658a00a1aca1378c2439
Muhamad Awad <muhamad@restate.dev>
2025-03-05 16:54:01 +0100

    Use the metadata proxy in restatectl
    Summary:
    Always use the metadata proxy in restatectl instead
    of the metadata client. This way metadata operation
    is always agnostic to the actual store kind that is
    being used by the server(s)

--- tools/restatectl/src/commands/node/remove_nodes.rs
+++ tools/restatectl/src/commands/node/remove_nodes.rs
@@ -9,19 +9,20 @@
 // by the Apache License, Version 2.0.
 
 use crate::commands::node::disable_node_checker::DisableNodeChecker;
-use crate::connection::ConnectionInfo;
+use crate::connection::{ConnectionInfo, NodeOperationError};
 use anyhow::Context;
 use clap::Parser;
 use cling::{Collect, Run};
 use itertools::Itertools;
 use restate_cli_util::c_println;
-use restate_core::metadata_store::MetadataStoreClient;
-use restate_metadata_server::create_client;
+use restate_core::metadata_store::serialize_value;
+use restate_core::protobuf::metadata_proxy_svc::PutRequest;
+use restate_core::protobuf::metadata_proxy_svc::metadata_proxy_svc_client::MetadataProxySvcClient;
 use restate_types::PlainNodeId;
-use restate_types::config::{MetadataClientKind, MetadataClientOptions};
 use restate_types::metadata::Precondition;
 use restate_types::metadata_store::keys::NODES_CONFIG_KEY;
-use restate_types::nodes_config::{NodesConfiguration, Role};
+use tonic::Code;
+use tonic::codec::CompressionEncoding;
 
 #[derive(Run, Parser, Collect, Clone, Debug)]
 #[clap(alias = "rm")]
@@ -38,7 +39,6 @@ pub async fn remove_nodes(
     opts: &RemoveNodesOpts,
 ) -> anyhow::Result<()> {
     let nodes_configuration = connection.get_nodes_configuration().await?;
-    let metadata_client = create_metadata_client(&nodes_configuration).await?;
     let logs = connection.get_logs().await?;
 
     let disable_node_checker = DisableNodeChecker::new(nodes_configuration, logs);
@@ -58,12 +58,30 @@ pub async fn remove_nodes(
 
     updated_nodes_configuration.increment_version();
 
-    metadata_client
-        .put(
-            NODES_CONFIG_KEY.clone(),
-            &updated_nodes_configuration,
-            Precondition::MatchesVersion(nodes_configuration.version()),
-        )
+    let request = PutRequest {
+        key: NODES_CONFIG_KEY.to_string(),
+        value: Some(
+            serialize_value(&updated_nodes_configuration)
+                .context("Failed to serialize logs")?
+                .into(),
+        ),
+        precondition: Some(Precondition::MatchesVersion(nodes_configuration.version()).into()),
+    };
+
+    connection
+        .try_each(None, |channel| async {
+            let mut client = MetadataProxySvcClient::new(channel)
+                .accept_compressed(CompressionEncoding::Gzip)
+                .send_compressed(CompressionEncoding::Gzip);
+
+            client.put(request.clone()).await.map_err(|err| {
+                if err.code() == Code::FailedPrecondition {
+                    NodeOperationError::Terminal(err)
+                } else {
+                    NodeOperationError::RetryableElsewhere(err)
+                }
+            })
+        })
         .await?;
 
     c_println!(
@@ -72,30 +90,4 @@ pub async fn remove_nodes(
     );
 
     Ok(())
-}
-
-async fn create_metadata_client(
-    nodes_configuration: &NodesConfiguration,
-) -> anyhow::Result<MetadataStoreClient> {
-    // find metadata nodes
-    let addresses: Vec<_> = nodes_configuration
-        .iter_role(Role::MetadataServer)
-        .map(|(_, config)| config.address.clone())
-        .collect();
-    if addresses.is_empty() {
-        return Err(anyhow::anyhow!(
-            "No nodes are configured to run metadata-server role, this command only \
-             supports replicated metadata deployment"
-        ));
-    }
-
-    // todo make this work with other metadata client kinds as well; maybe proxy through Restate server
-    let metadata_store_client_options = MetadataClientOptions {
-        kind: MetadataClientKind::Replicated { addresses },
-        ..Default::default()
-    };
-
-    create_client(metadata_store_client_options)
-        .await
-        .map_err(|e| anyhow::anyhow!("Failed to create metadata store client: {}", e))
 }
