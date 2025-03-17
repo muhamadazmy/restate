b18a3ea06ca7054ddd82d0c891acc026487b4352
Muhamad Awad <muhamad@restate.dev>
2025-03-17 12:56:23 +0100

    Datafusion Logs table
    Summary:
    Expose the logs metadata over datafusion

---
+++ crates/storage-query-datafusion/src/log/schema.rs
@@ -0,0 +1,51 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+#![allow(dead_code)]
+
+use datafusion::arrow::datatypes::DataType;
+
+use crate::table_macro::*;
+
+define_table!(
+    log(
+    /// Log ID
+    id: DataType::UInt32,
+
+    /// Segment index
+    segment_index: DataType::UInt32,
+
+    /// Segment start lsn
+    base_lsn: DataType::UInt64,
+
+    /// Segment provider kind
+    kind: DataType::Utf8,
+
+    // replicated loglet specific params
+
+    /// Loglet ID
+    loglet_id: DataType::UInt64,
+
+    // todo: should this be merged into
+    // a utf8 `N1:1` for example?
+
+    /// Node id of the node running the sequencer
+    seq_node_id: DataType::UInt32,
+
+    /// Node generation of the node running the sequencer
+    seq_node_gen: DataType::UInt32,
+
+    /// loglet replication factor
+    replication: DataType::Utf8,
+
+    /// Loglet log server
+    log_server: DataType::UInt32,
+
+));
