d7b8e8015ae7dd82e4494f55987af9be446f6b6c
Muhamad Awad <muhamad@restate.dev>
2025-03-19 12:51:07 +0100

    Datafusion NodeState table
    Summary:
    The `cluster_node_state` reflects the actual observed node state
    as seen by this cluster controller node
    
    A quick view of the cluster node can be retrieved as
    
    ```
    restatectl sql "select n.*,s.* from cluster_node as n left join cluster_node_state s on n.id=s.id order by n.id"
    
     ID  GENERATION  NAME    ADDRESS                 LOCATION  ADMIN_ROLE  WORKER_ROLE  METADATA_SERVER_ROLE  LOG_SERVER_ROLE  INGRESS_ROLE  STORAGE_STATE  METADATA_SERVER_STATE  ID  STATUS  UPTIME  LAST_SEEN_TS   LAST_ATTEMPT_TS
     1   24          node-1  http://127.0.0.1:5122/            true        true         true                  true             false         read-write     member                 1   ALIVE   261     1742374090664
     2   20          node-2  http://127.0.0.1:6122/            true        true         true                  true             false         read-write     member                 2   ALIVE   258     1742374090665
     3   19          node-3  http://127.0.0.1:7122/            true        true         false                 false            false                                               3   DEAD            1742373946665
    
    3 rows. Query took 49.954471ms
    ```

---
+++ crates/storage-query-datafusion/src/node_state/row.rs
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+use super::schema::NodeStateBuilder;
+use restate_types::{PlainNodeId, cluster::cluster_state::NodeState};
+
+const STATE_ALIVE: &str = "ALIVE";
+const STATE_DEAD: &str = "DEAD";
+const STATE_SUSPECT: &str = "SUSPECT";
+
+#[inline]
+pub(crate) fn append_node_row(
+    builder: &mut NodeStateBuilder,
+    node_id: PlainNodeId,
+    node_state: &NodeState,
+) {
+    let mut row = builder.row();
+    row.id(node_id.into());
+
+    match node_state {
+        NodeState::Alive(alive) => {
+            row.status(STATE_ALIVE);
+            row.uptime(alive.uptime.as_secs());
+            row.last_seen_ts(alive.last_heartbeat_at.as_u64());
+            row.generation(alive.generational_node_id.generation());
+        }
+        NodeState::Dead(dead) => {
+            row.status(STATE_DEAD);
+            if let Some(ts) = dead.last_seen_alive {
+                row.last_seen_ts(ts.as_u64());
+            }
+        }
+        NodeState::Suspect(suspect) => {
+            row.status(STATE_SUSPECT);
+            row.last_attempt_ts(suspect.last_attempt.as_u64());
+            row.generation(suspect.generational_node_id.generation());
+        }
+    }
+}
