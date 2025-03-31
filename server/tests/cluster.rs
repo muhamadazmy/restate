be782e627343a972eefe0704b19c9d768d576c66
Muhamad Awad <muhamad@restate.dev>
2025-03-31 14:52:07 +0200

    Properly handle unset partition-replication in restatectl
    Summary:
    Ensure correct behavior when the partition-replication flag is omitted in both provision and config set.
    Previously, an unset flag defaulted to everywhere, which was incorrect.
    
    - In provision, if the flag is omitted, the cluster defaults to the configured default-partition-replication.
     The user can override this by specifying a valid replication property.
    
     - In config set, omitting the flag leaves the current cluster configuration unchanged.

--- server/tests/cluster.rs
+++ server/tests/cluster.rs
@@ -26,7 +26,6 @@ use restate_types::config::{MetadataServerKind, RaftOptions};
 use restate_types::logs::metadata::{
     NodeSetSize, ProviderConfiguration, ProviderKind, ReplicatedLogletConfig,
 };
-use restate_types::partition_table::PartitionReplication;
 use restate_types::replication::ReplicationProperty;
 use restate_types::{config::Configuration, nodes_config::Role};
 use std::convert::Infallible;
@@ -72,7 +71,7 @@ async fn replicated_loglet() -> googletest::Result<()> {
     cluster.nodes[0]
         .provision_cluster(
             None,
-            PartitionReplication::Everywhere,
+            ReplicationProperty::new(NonZeroU8::new(3).expect("is non zero")),
             Some(ProviderConfiguration::Replicated(replicated_loglet_config)),
         )
         .await
