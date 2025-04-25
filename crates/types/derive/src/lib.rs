09a7acf2ce9d7c94f5546c4793b1ce9083b37a8a
Muhamad Awad <muhamad@restate.dev>
2025-04-25 13:49:57 +0200

    Derive BilrostNewType to use with all ID types
    Summary:
    This makes sure that new types like `Version`, `PartitionID`, `PlaintNodeID`, etc.. are flattened
    inside a bilrost message insteada of creating them as their own message.

---
+++ crates/types/derive/src/lib.rs
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 - 2025 Restate Software, Inc., Restate GmbH.
+// All rights reserved.
+//
+// Use of this software is governed by the Business Source License
+// included in the LICENSE file.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0.
+
+extern crate proc_macro;
+
+mod flatten;
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(BilrostNewType)]
+pub fn bilrost_new_type(item: TokenStream) -> TokenStream {
+    flatten::new_type(item)
+}
