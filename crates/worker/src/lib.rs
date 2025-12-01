50ddb76b315ed70ec7cedebcc607a692a16efc77
Muhamad Awad <muhamad@restate.dev>
2025-12-01 12:04:56 +0100

    Use ingress-client in the `Shuffler`
    Avoid direct writes to bifrost in shuffler by using a
    dedicated ingress-client instance.

--- crates/worker/src/lib.rs
+++ crates/worker/src/lib.rs
@@ -19,9 +19,13 @@ mod partition_processor_manager;
 mod subscription_controller;
 mod subscription_integration;
 
+use std::num::NonZeroUsize;
 use std::sync::Arc;
 
 use codederror::CodedError;
+use restate_core::network::Swimlane;
+use restate_ingestion_client::SessionOptions;
+use restate_wal_protocol::Envelope;
 use tracing::info;
 
 use restate_bifrost::Bifrost;
@@ -52,7 +56,6 @@ use restate_types::health::HealthStatus;
 use restate_types::partitions::state::PartitionReplicaSetStates;
 use restate_types::protobuf::common::WorkerStatus;
 use restate_types::schema::subscriptions::SubscriptionResolver;
-use restate_wal_protocol::Envelope;
 
 use crate::partition::invoker_storage_reader::InvokerStorageReader;
 use crate::partition_processor_manager::PartitionProcessorManager;
@@ -98,7 +101,7 @@ pub struct Worker<T> {
     datafusion_remote_scanner: RemoteQueryScannerServer,
     ingress_kafka: IngressKafkaService<T>,
     subscription_controller_handle: SubscriptionControllerHandle,
-    partition_processor_manager: PartitionProcessorManager,
+    partition_processor_manager: PartitionProcessorManager<T>,
 }
 
 impl<T> Worker<T>
@@ -142,6 +145,18 @@ where
             )));
         }
 
+        // A dedicated ingestion client for PPM that uses
+        // BifrostData swimlane
+        let ppm_ingestion_client = IngestionClient::new(
+            networking.clone(),
+            Metadata::with_current(|m| m.updateable_partition_table()),
+            partition_routing.clone(),
+            NonZeroUsize::new(1024 * 1024).unwrap(), // 1MB
+            Some(SessionOptions {
+                swimlane: Swimlane::BifrostData,
+                ..Default::default()
+            }),
+        );
         let partition_processor_manager = PartitionProcessorManager::new(
             health_status,
             Configuration::live(),
@@ -156,6 +171,7 @@ where
             )
             .await
             .map_err(BuildError::SnapshotRepository)?,
+            ppm_ingestion_client,
         );
 
         let remote_scanner_manager = RemoteScannerManager::new(
