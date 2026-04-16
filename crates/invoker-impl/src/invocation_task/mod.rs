3baaeb10935c9522e210d0eb310853b85a23a90e
Muhamad Awad <muhamad@restate.dev>
2026-04-16 13:25:59 +0200

    [service-protocol] Add support for suspension message V7
    Summary:
    This adds support to handle which suspension message to decode
    based on the protocol version. It also introduce the Suspension message V7
    based on the new protocol.proto schema

--- crates/invoker-impl/src/invocation_task/mod.rs
+++ crates/invoker-impl/src/invocation_task/mod.rs
@@ -86,6 +86,10 @@ const SERVICE_PROTOCOL_VERSION_V6: HeaderValue =
     HeaderValue::from_static("application/vnd.restate.invocation.v6");
 
 #[allow(clippy::declare_interior_mutable_const)]
+const SERVICE_PROTOCOL_VERSION_V7: HeaderValue =
+    HeaderValue::from_static("application/vnd.restate.invocation.v7");
+
+#[allow(clippy::declare_interior_mutable_const)]
 const X_RESTATE_SERVER: HeaderName = HeaderName::from_static("x-restate-server");
 
 /// Collects state entries from an [`EagerState`] stream, respecting a size limit.
@@ -600,6 +604,7 @@ fn service_protocol_version_to_header_value(
         ServiceProtocolVersion::V4 => SERVICE_PROTOCOL_VERSION_V4,
         ServiceProtocolVersion::V5 => SERVICE_PROTOCOL_VERSION_V5,
         ServiceProtocolVersion::V6 => SERVICE_PROTOCOL_VERSION_V6,
+        ServiceProtocolVersion::V7 => SERVICE_PROTOCOL_VERSION_V7,
     }
 }
 
